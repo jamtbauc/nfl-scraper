37b36480b84c1b399ce6c78958e130ea0df22a7d
James Baucum <jbaucum@imcc.com>
2022-10-03 13:49:46 -0500

    Use python str.find instead of beautiful soup

--- boxscores/Game.py
+++ boxscores/Game.py
@@ -1,46 +1,48 @@
 from datetime import datetime
+import numbers
+from re import template
 from Player import Player
 
 class Game:
     ### dictionary with abbrev as key and corresponding team as the value
     abbrevs = {
-        "ARI": "Arizona Cardinals",
-        "ATL": "Atlanta Falcons",
-        "BAL": "Baltimore Ravens",
-        "BUF": "Buffalo Bills",
-        "CAR": "Carolina Panthers",
-        "CHI": "Chicago Bears",
-        "CIN": "Cincinnati Bengals",
-        "CLE": "Cleveland Browns",
-        "DAL": "Dallas Cowboys",
-        "DEN": "Denver Broncos",
-        "DET": "Detroit Lions",
-        "GNB": "Green Bay Packers",
-        "HOU": "Houston Texans",
-        "IND": "Indianapolis Colts",
-        "JAX": "Jacksonville Jaguars",
-        "KAN": "Kansas City Chiefs",
-        "LAC": "Los Angeles Chargers",
-        "LAR": "Los Angeles Rams",
-        "LVR": "Las Vegas Raiders",
-        "MIA": "Miami Dolphins",
-        "MIN": "Minnesota Vikings",
-        "NOR": "New Orleans Saints",
-        "NWE": "New England Patriots",
-        "NYG": "New York Giants",
-        "NYJ": "New York Jets",
-        "PHI": "Philadelphia Eagles",
-        "PIT": "Pittsburgh Steelers",
-        "SEA": "Seattle Seahawks",
-        "SFO": "San Francisco 49ers",
-        "TAM": "Tampa Bay Buccaneers",
-        "TEN": "Tennessee Titans",
-        #### WASHINGTON NAME LIST
-        "WAS": {2021: "Washington Football Team", 2022: "Washington Commanders"}
+        "Arizona Cardinals": "ARI",
+        "Atlanta Falcons": "ATL",
+        "Baltimore Ravens": "BAL",
+        "Buffalo Bills": "BUF",
+        "Carolina Panthers": "CAR",
+        "Chicago Bears": "CHI",
+        "Cincinnati Bengals": "CIN",
+        "Cleveland Browns": "CLE",
+        "Dallas Cowboys": "DAL",
+        "Denver Broncos": "DEN",
+        "Detroit Lions": "DET",
+        "Green Bay Packers": "GNB",
+        "Houston Texans": "HOU",
+        "Indianapolis Colts": "IND",
+        "Jacksonville Jaguars": "JAX",
+        "Kansas City Chiefs": "KAN",
+        "Los Angeles Chargers": "LAC",
+        "Los Angeles Rams": "LAR",
+        "Las Vegas Raiders": "LVR",
+        "Miami Dolphins": "MIA",
+        "Minnesota Vikings": "MIN",
+        "New Orleans Saints": "NOR",
+        "New England Patriots": "NWE",
+        "New York Giants": "NYG",
+        "New York Jets": "NYJ",
+        "Philadelphia Eagles": "PHI",
+        "Pittsburgh Steelers": "PIT",
+        "Seattle Seahawks": "SEA",
+        "San Francisco 49ers": "SFO",
+        "Tampa Bay Buccaneers": "TAM",
+        "Tennessee Titans": "TEN",
+        "Washington Football Team": "WAS",
+        "Washington Commanders": "WAS"
     }
     
-    def __init__(self, away, home, date):
-        self.__attendance = ""
+    def __init__(self, date, away, home):
+        self.__attendance = 0
         self.__away = away
         self.__away_coach = ""
         self.__away_def_players = []
@@ -56,74 +58,9 @@ class Game:
         self.__home_starters = {}
         self.__officials = {}
         self.__stadium = ""
-        
-    ### SETTER METHODS SORTED ALPHABETICALLY
-    def set_away_coach(self, coach):
-        self.__away_coach = coach
-           
-    def set_away_score(self, score):
-        self.__away_score = score
-        
-    def set_home_coach(self, coach):
-        self.__home_coach = coach
-        
-    def set_home_score(self, score):
-        self.__home_score = score
-       
-    ### GETTER METHODS SORTED ALPHABETICALLY
-    def get_attendance(self):
-        return self.__attendance
-    
-    def get_away(self):
-        return self.__away
-    
-    def get_away_coach(self):
-        return self.__away_coach
-    
-    def get_away_score(self):
-        return self.__away_score
-    
-    def get_date(self):
-        return self.__date
-    
-    def get_home(self):
-        return self.__home
-    
-    def get_home_coach(self):
-        return self.__home_coach
-    
-    def get_home_score(self):
-        return self.__home_score
-    
-    def get_stadium(self):
-        return self.__stadium
     
     ### HELPER METHODS  
-    ##### PRIVATE HELPERS
-    def __add_off_player(self, player):
-        abbrev = player.get_team()
-        
-        if abbrev != "":
-            if abbrev in self.abbrevs:
-                team = self.abbrevs[abbrev]
-                
-                if team == self.__home:
-                    self.__home_off_players.append(player)
-                elif team == self.__away:
-                    self.__away_off_players.append(player)
-                    
-    def __add_def_player(self, player):
-        abbrev = player.get_team()
-        
-        if abbrev != "":
-            if abbrev in self.abbrevs:
-                team = self.abbrevs[abbrev]
-                
-                if team == self.__home:
-                    self.__home_def_players.append(player)
-                elif team == self.__away:
-                    self.__away_def_players.append(player)
-        
+    ##### PRIVATE HELPERS  
     def __extract_time(self, time_str):
         am_pm = time_str[-2:]
         if am_pm == "am":
@@ -137,89 +74,88 @@ class Game:
         date_str += " " + time_str
         
         self.__date = self.__date.strptime(date_str, "%B %m, %Y %I:%M%p")
+
+    def __extract_score_coach(self, info):
+        # trim unwanted info
+        start = info.find('<strong')
+        info = info[start:]
+        # find which team the info is for
+        start = info.find('.htm">') + 6
+        stop = info.find('</a>')
+        team = info[start:stop]
+        info = info[stop:]
+        # find next score info
+        start = info.find('<div class="score">')
+        info = info[start:]
+        stop = info.find('</div>')
+        score = info[19:stop]
+        # find next coach info
+        start = info.find('<div class="datapoint"')
+        info = info[start:]
+        start = info.find('.htm">') + 6
+        stop = info.find('</a>')
+        coach = info[start:stop]
+        info = info[stop:]
+        # assign score and coach to team
+        if team == self.__away:
+            self.__away_score = score
+            self.__away_coach = coach
+        elif team == self.__home:
+            self.__home_score = score
+            self.__home_coach = coach
+
+        return info
     
     ##### PUBLIC HELPERS  
-    def extract_coaches(self, coaches):
-        for c in coaches:
-            coach = c.text[7:]
-            if self.__home_coach == "":
-                self.__home_coach = coach
-            else:
-                self.__away_coach = coach
-                
-    def extract_player_stats(self, stats_soup, type):
-        table = stats_soup.find("tbody")
-        stats = table.find_all("tr")
-        for row in stats:
-            player = row.find("th")
-            player = Player(player.text)
-            
-            nums = row.find_all("td")
-            
-            for n in nums:
-                cat = n.get("data-stat")
-                stat = n.text
-                
-                player.add_stat(cat, stat)
-                
-            if type == "Offense":
-                self.__add_off_player(player)
-            else:
-                self.__add_def_player(player)
-            
-    def extract_officials(self, official_soup):
-        ref_positions = official_soup.find_all("th")
-        refs = official_soup.find_all("a")
-        for i in range(len(ref_positions)):
-            pos = ref_positions[i].text
-            ref = refs[i].text
-            if pos not in self.__officials:
-                self.__officials[pos] = ""
-            self.__officials[pos] = ref
-                
-    def extract_scorebox_meta(self, game_data):
-        search_str = game_data.text
-        time_idx = search_str.index("Start Time: ")
-        stadium_idx = search_str.index("Stadium: ")
-        attendance_idx = search_str.index("Attendance: ")
-        length_idx = search_str.index("Time of Game: ")
-        
-        self.__extract_time(search_str[time_idx+12:stadium_idx])
-        
-        self.__stadium = search_str[stadium_idx+9:attendance_idx]  
-        
-        attendance = search_str[attendance_idx+12:length_idx]
-        attendance = attendance.replace(',', '')
-        self.__attendance = int(attendance)
+    def extract_score_plays(self, text):
+        text_start = text.find('<div class="content_grid"')
+        scores = text[:text_start]
 
-    def extract_scores(self, scores):
-        for s in scores:
-            if self.__home_score < 0:
-                self.__home_score = int(s.text)
-            else:
-                self.__away_score = int(s.text)
-    
-    def extract_starters(self, starter_soup, type):
-        table = starter_soup.find("tbody")
-        players = table.find_all("a")
-        positions = table.find_all("td")
-        
-        for i in range(len(players)):
-            pos = positions[i].text
-            player = players[i].text
-            if type == "Home":
-                if pos not in self.__home_starters:
-                    self.__home_starters[pos] = []
+        print(scores)
+
+        return text[text_start:]
                 
-                self.__home_starters[pos].append(player)
-            else:
-                if pos not in self.__away_starters:
-                    self.__away_starters[pos] = []
-                
-                self.__away_starters[pos].append(player)
+    def extract_scorebox_meta(self, text):
+        beg = text.index('</strong>: ')
+        text_break = text.find('<div class="linescore_wrap">')
+        meta = text[beg:text_break]
+        meta_end = text.find('<style>')
+        meta = meta[:meta_end]
+        # extract time from scorebox meta
+        start = meta.find('</strong>: ') + 11
+        end = meta.find('</div>')
+        self.__extract_time(meta[start:end])
+        meta = meta[end:]
+        # extract stadium
+        start = meta.find('.htm">') + 6
+        end = meta.find('</a>')
+        self.__stadium = meta[start:end]
+        meta = meta[end+4:]
+        # extract attendance
+        start = meta.find('.htm">') + 6
+        end = meta.find('</a>')
+        att = meta[start:end]
+        self.__attendance = int(att.replace(',', ''))
+        meta = meta[end+4:]
+
+        return text[text_break:]
+
+    def extract_scorebox(self, text):
+        # trim info
+        s_idx = text.find('<div class="scorebox')
+        e_idx = text.find('<div class="scorebox_meta')
+        info = text[s_idx:e_idx]
+        # extract info and trim
+        info = self.__extract_score_coach(info)
+        # repeat for next team
+        info = self.__extract_score_coach(info)
+
+        return text[e_idx:]
                 
     def print_game_info(self):
-        print(f"{self.__date}: {self.__away}-{self.__away_score} at {self.__home}-{self.__home_score}")
+        print(f"{self.__date}: {self.__stadium} | {self.__attendance} in attendance")
+
+        print(f"{self.__away}-{self.__away_score} at {self.__home}-{self.__home_score}")
         
         print(f"{self.__away_coach} vs {self.__home_coach}")
         
--- boxscores/boxscore_scraper.py
+++ boxscores/boxscore_scraper.py
@@ -1,3 +1,6 @@
+from cgitb import html, text
+from itertools import tee
+from sqlite3 import Date
 from bs4 import BeautifulSoup
 from bs4 import SoupStrainer as strainer
 from bs4 import Comment
@@ -8,16 +11,28 @@ import http.client
 import re
 from time import time
 
-def extract_basic_info(data):
-        away = data[0]
-        
-        rem  = data[1]
-        rem = rem.split(" - ")
-        home = rem[0]
-        date = remove_date_formals(rem[1])
-        date = datetime.strptime(date, "%B %m, %Y")
-        
-        return away, date, home
+def extract_basic_info(text):
+    idx = text.find("<h1>")
+    e_idx = text.find("</h1>")
+    info = text[idx+4:e_idx]
+    # split title into teams and date
+    info = info.split(" - ")
+    # separate into name vars
+    matchup = info[0]
+    matchup = matchup.split(" at ")
+    away = matchup[0]
+    home = matchup[1]
+    date = info[1]
+    date = remove_date_formals(date)
+    date = datetime.strptime(date, "%B %d, %Y")
+    # create game object
+    game = Game(date, away, home)
+    # remove used data from text
+    text = text[e_idx:]
+    idx = text.find("<div")
+    text = text[idx:]
+
+    return text, game
         
 def remove_date_formals(date):
     formals = ("st", "nd", "rd", "th")
@@ -42,6 +57,21 @@ def request_data(base, url):
         
         #return html.decode('ISO-8859-1')
         return html.decode("utf-8", "replace")
+
+def trim_text(text, start, end):
+    start_idx = text.find(start)
+    end_idx = text.find(end)
+
+    return text[start_idx:end_idx]
+
+### REMOVE WHEN DEV IS DONE
+def open_html(file):
+    with open(file) as html:
+        text = html.read()
+
+        text = bytes(text, "utf-8")
+        return text.decode("utf-8", "replace")
+
     
 def run_scraper():
     games = {}
@@ -49,59 +79,22 @@ def run_scraper():
     base = "www.pro-football-reference.com"
     url = "/boxscores/202109090tam.htm"
     # using defined base and request urls, get decoded data
-    bytes = request_data(base, url)
-    # create soup
-    head = strainer("head")
-    soup = BeautifulSoup(bytes, 'html.parser', parse_only=head)
-    title = soup.title.text[:-29]
-    
-    # extract away team, game date, and home team from webpage title
-    away, date, home = extract_basic_info(title.split(" at "))
-    
-    # create new game object
-    game = Game(away, home, date)
-    
-    # extract away score and home score
-    # create strainer to save memory by parsing only scorebox div
-    scorebox = strainer(class_="scorebox")
-    # create soup using strainer (reduces amount of code bs4 has to parse)
-    scorebox_soup = BeautifulSoup(bytes, 'html.parser', parse_only=scorebox)
-    game.extract_scores(scorebox_soup.find_all(class_="score"))
-    
-    # extract away coach and home coach
-    game.extract_coaches(scorebox_soup.find_all(class_="datapoint"))
-    
-    # extract attendance, stadium, and start time
-    game.extract_scorebox_meta(scorebox_soup.find(class_="scorebox_meta"))
-    
-    # extract offensive stats for both teams
-    # create new strainer to only parse all_player_offense div
-    offense = strainer(id="div_player_offense")
-    offense_soup = BeautifulSoup(bytes, 'html.parser', parse_only=offense)
-    game.extract_player_stats(offense_soup, "Offense")
-    
-    # webpage uses javascript to populate some tables
-    # parse comments to extract needed data
-    ## create new strainer to only parse main div code
-    main_soup = BeautifulSoup(bytes, 'html.parser')
-    comments = main_soup.find_all(text=lambda text: isinstance(text, Comment))
-    for comment in comments:
-        if "div_officials" in comment:
-            official_soup = BeautifulSoup(comment, 'html.parser')
-            game.extract_officials(official_soup)
-        elif "div_player_defense" in comment:
-            defense_soup = BeautifulSoup(comment, 'html.parser')
-            game.extract_player_stats(defense_soup, "Defense")
-        elif "div_home_starters" in comment:
-            starter_soup = BeautifulSoup(comment, 'html.parser')
-            game.extract_starters(starter_soup, "Home")
-        elif "div_vis_starters" in comment:
-            starter_soup = BeautifulSoup(comment, 'html.parser')
-            game.extract_starters(starter_soup, "Away")
-            
-    with open('test_game.csv', 'w', newline='') as file:
-        writer = csv.writer(file, delimiter=',')
-        writer.writerow(game.get_game_as_list())
+    # bytes = request_data(base, url)
+    text = open_html("./page.html")
+    # trim html to content we need
+    text = trim_text(text, '<div id="content"', '<div id="footer"')
+    # extract game date string, home and away teams
+    text, game = extract_basic_info(text)
+    # extract scores and coaches
+    text = game.extract_scorebox(text)
+    # extract scorebox meta
+    text = game.extract_scorebox_meta(text)
+    # extract linescores
+    text = game.extract_score_plays(text)
+
+    print(text[:30])
+    game.print_game_info() 
+
         
 if __name__ == "__main__":
     start_time = time()
