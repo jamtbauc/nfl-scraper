69705a7a4a52c31eddca77a661218183997a9c2a
James Baucum <jamestbauc@gmail.com>
2022-11-27 16:24:52 -0600

    Remove if statement in main loop

--- boxscores/boxscore_scraper.py
+++ boxscores/boxscore_scraper.py
@@ -16,6 +16,7 @@ def get_urls(file):
 
 # Send get request to url specified at base website. Returns response body
 def request_data(conn, url):
+        print(url)
         # send GET request
         conn.request("GET", url)
         # retrieve response body from GET request
@@ -42,46 +43,45 @@ def run_scraper():
     # loop through all urls
     last_run = time()
     for url in urls:
-        if "/boxscores/201809090crd.htm" in url:
-            print(url)
-            # pro-football-reference limits to 20 request per minute. Ensure request only happens every 3 seconds
-            if (time() - last_run) < 3:
-                diff = 3.01 - (time() - last_run)
-                print(f"Sleeping for {diff} seconds")
-                sleep(diff)
-                last_run = time()
-            # using defined base and request urls, get decoded data   
-            bytes = request_data(conn, url)
-            # trim html to content we need
-            text = trim_text(bytes, '<div id="content"', '<div id="footer"')
-            ### Declare the sections we need to parse
-            parser.setMatchup(trim_text(text, '<div id="content" role="main" class="box">', '<div class="game_summary nohover current">'))
-            parser.setScorebox(trim_text(text, '<div class="scorebox">', '<div class="scorebox_meta">'))
-            parser.setScoreboxMeta(trim_text(text, '<div class="scorebox_meta">', '<style>'))
-            parser.setAllScoring(trim_text(text, '<div class="table_container" id="div_scoring">', '<div class="content_grid">'))
-            parser.setGameInfo(trim_text(text, '<div id="all_game_info"', '<div id="all_officials"'))
-            parser.setAllOfficials(trim_text(text, '<div id="all_officials"', '<div id="all_expected_points"'))
-            parser.setAllTeamStats(trim_text(text, '<div id="all_team_stats"', '<div id="all_player_offense"'))
-            parser.setAllPlayerOff(trim_text(text, '<div id="all_player_offense"', '<div id="all_player_defense"'))
-            parser.setAllPlayerDef(trim_text(text, '<div id="all_player_defense"', '<div id="all_returns"'))
-            parser.setAllReturns(trim_text(text, '<div id="all_returns"', '<div id="all_kicking"'))
-            parser.setAllKicking(trim_text(text, '<div id="all_kicking"', '<div id="all_passing_advanced"'))
-            parser.setPassingAdv(trim_text(text, '<div id="all_passing_advanced"', '<div id="all_rushing_advanced"'))
-            parser.setRushingAdv(trim_text(text, '<div id="all_rushing_advanced"', '<div id="all_receiving_advanced"'))
-            parser.setReceivingAdv(trim_text(text, '<div id="all_receiving_advanced"', '<div id="all_defense_advanced"'))
-            parser.setDefenseAdv(trim_text(text, '<div id="all_defense_advanced"', '<div id="all_home_starters"'))
-            parser.setHomeStarters(trim_text(text, '<div id="all_home_starters"', '<div id="all_vis_starters"'))
-            parser.setAwayStarters(trim_text(text, '<div id="all_vis_starters"', '<div id="all_home_snap_counts"'))
-            parser.setHomeSnaps(trim_text(text, '<div id="all_home_snap_counts"', '<div id="all_vis_snap_counts"'))
-            parser.setAwaySnaps(trim_text(text, '<div id="all_vis_snap_counts"', '<div id="all_home_drives"'))
-            parser.setHomeDrives(trim_text(text, '<div id="all_home_drives"', '<div id="all_vis_drives"'))
-            parser.setAwayDrives(trim_text(text, '<div id="all_vis_drives"', '<div id="all_pbp"'))
-            parser.setPlays(trim_text(text, '<div id="all_pbp"', '<div id="bottom_nav"'))
-            
-            game_date = parser.extract_basic_info()
-            today = datetime.today().date()
-            if game_date < today:
-                parser.parseGame()
+        # pro-football-reference limits to 20 request per minute. Ensure request only happens every 3 seconds
+        # if (time() - last_run) < 3:
+        #     diff = 3.01 - (time() - last_run)
+        #     print(f"Sleeping for {diff} seconds")
+        #     sleep(diff)
+        #     last_run = time()
+        # using defined base and request urls, get decoded data   
+        bytes = request_data(conn, url)
+        sleep(3.1)
+        # trim html to content we need
+        text = trim_text(bytes, '<div id="content"', '<div id="footer"')
+        ### Declare the sections we need to parse
+        parser.setMatchup(trim_text(text, '<div id="content" role="main" class="box">', '<div class="game_summary nohover current">'))
+        parser.setScorebox(trim_text(text, '<div class="scorebox">', '<div class="scorebox_meta">'))
+        parser.setScoreboxMeta(trim_text(text, '<div class="scorebox_meta">', '<style>'))
+        parser.setAllScoring(trim_text(text, '<div class="table_container" id="div_scoring">', '<div class="content_grid">'))
+        parser.setGameInfo(trim_text(text, '<div id="all_game_info"', '<div id="all_officials"'))
+        parser.setAllOfficials(trim_text(text, '<div id="all_officials"', '<div id="all_expected_points"'))
+        parser.setAllTeamStats(trim_text(text, '<div id="all_team_stats"', '<div id="all_player_offense"'))
+        parser.setAllPlayerOff(trim_text(text, '<div id="all_player_offense"', '<div id="all_player_defense"'))
+        parser.setAllPlayerDef(trim_text(text, '<div id="all_player_defense"', '<div id="all_returns"'))
+        parser.setAllReturns(trim_text(text, '<div id="all_returns"', '<div id="all_kicking"'))
+        parser.setAllKicking(trim_text(text, '<div id="all_kicking"', '<div id="all_passing_advanced"'))
+        parser.setPassingAdv(trim_text(text, '<div id="all_passing_advanced"', '<div id="all_rushing_advanced"'))
+        parser.setRushingAdv(trim_text(text, '<div id="all_rushing_advanced"', '<div id="all_receiving_advanced"'))
+        parser.setReceivingAdv(trim_text(text, '<div id="all_receiving_advanced"', '<div id="all_defense_advanced"'))
+        parser.setDefenseAdv(trim_text(text, '<div id="all_defense_advanced"', '<div id="all_home_starters"'))
+        parser.setHomeStarters(trim_text(text, '<div id="all_home_starters"', '<div id="all_vis_starters"'))
+        parser.setAwayStarters(trim_text(text, '<div id="all_vis_starters"', '<div id="all_home_snap_counts"'))
+        parser.setHomeSnaps(trim_text(text, '<div id="all_home_snap_counts"', '<div id="all_vis_snap_counts"'))
+        parser.setAwaySnaps(trim_text(text, '<div id="all_vis_snap_counts"', '<div id="all_home_drives"'))
+        parser.setHomeDrives(trim_text(text, '<div id="all_home_drives"', '<div id="all_vis_drives"'))
+        parser.setAwayDrives(trim_text(text, '<div id="all_vis_drives"', '<div id="all_pbp"'))
+        parser.setPlays(trim_text(text, '<div id="all_pbp"', '<div id="bottom_nav"'))
+        
+        game_date = parser.extract_basic_info()
+        today = datetime.today().date()
+        if game_date < today:
+            parser.parseGame()
                 
     # close http connection
     conn.close()
